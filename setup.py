f1f11a6ae8cc37af479b4d4652700bd91963c6d1
FBumann <117816358+FBumann@users.noreply.github.com>
2024-02-07 18:18:24 +0100

    Update setup.py and refine requirements
    The setup.py script is updated to include additional package metadata such as author details, description, and related links. Also, version constraints are added to the packages listed in requirements.txt to ensure that appropriate versions of dependencies are installed.

--- setup.py
+++ setup.py
@@ -7,6 +7,25 @@ def read_requirements(file_path):
 setup(
     name='flixOpt',
     version='0.1.0',
+    author='Felix Bumann',
+    author_email='felixbumann387@gmail.com',
+    description='Vector based energy and material flow optimization framework in python.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/flixOpt',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        'Topic :: Scientific/Engineering :: Mathematics :: Numerical Analysis :: Optimization',
+        'Topic :: Software Development :: Libraries :: Python'
+    ],
+
     packages=find_packages(exclude=['Tests', 'docs', '.git', '.vscode', 'build', '.venv']),
     install_requires=read_requirements('requirements.txt')
 )

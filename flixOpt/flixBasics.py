3f109b754019c925e8cc607841467205681cbb74
FBumann <117816358+FBumann@users.noreply.github.com>
2024-02-08 16:25:57 +0100

    Added __repr__ to cTS_Vector and cME for better useability

--- flixOpt/flixBasics.py
+++ flixOpt/flixBasics.py
@@ -102,7 +102,7 @@ class cTS_vector:
   
     @property
     def label_full(self):
-      return self.owner.label_full + '_' + self.label    
+      return self.owner.label_full + '_' + self.label
       
           
     def __init__(self, label, value, owner):    
@@ -132,6 +132,15 @@ class cTS_vector:
       owner.TS_list.append(self)    
       
       self.weight_agg = 1 # weight for Aggregation method # between 0..1, normally 1
+
+    def __repr__(self):
+        text = f"{self.label} = "
+        if self.isscalar:
+            return text + f"{self.d_i}"
+        elif self.isArray:
+            return text + f"{self.d_i.min()}-{self.d_i.max()}"
+        else:
+            return text + f"None"
   
     @staticmethod
     def __makeSkalarIfPossible(d):        
